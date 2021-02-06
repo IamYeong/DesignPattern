20a6dfe30944bfa8408af11a89e4d3b0a0343b1e
UNKN0WN-Y <wjdrhkddud2@gmail.com>
2021-02-06 20:03:15 +0900

    complete factory pattern

--- app/src/main/java/com/todaylunch/testproject/FactoryActivity.java
+++ app/src/main/java/com/todaylunch/testproject/FactoryActivity.java
@@ -3,25 +3,96 @@ package com.todaylunch.testproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class FactoryActivity extends AppCompatActivity {
 
     private Button btn;
     private Spinner spr;
-    private ArrayAdapter<String> arrayAdapter;
-    private ArrayList<String> arrayList;
+    private TextView tv;
+    private ArrayAdapter<Integer> arrayAdapter;
+    private ArrayList<Integer> arrayList;
+    private Factory factory = null;
+    private String type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_factory);
 
+        btn = findViewById(R.id.btn_factory);
+        spr = findViewById(R.id.spr_factory);
+        tv = findViewById(R.id.tv_factory);
+
+        setSpinner(3);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                tv.setText(type + ", " + factory.toString());
+
+            }
+        });
+
+        spr.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                switch(position) {
+
+                    case 0 :
+                        factory = CreationFactory.BuildFactory(FactoryType.Type1);
+                        type = "ONE";
+                        break;
+
+                    case 1 :
+                        factory = CreationFactory.BuildFactory(FactoryType.Type2);
+                        type = "TWO";
+                        break;
+
+                    case 2 :
+                        factory = CreationFactory.BuildFactory(FactoryType.Type3);
+                        type = "THREE";
+                        break;
+
+                }
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 
 
     }
+
+    private void setSpinner(int spinnerCount) {
+
+        arrayList = new ArrayList<>();
+
+
+        for (int i = 0; i < spinnerCount; i++) {
+
+            arrayList.add(i);
+
+        }
+
+        arrayAdapter = new ArrayAdapter<Integer>(this, R.layout.support_simple_spinner_dropdown_item, arrayList);
+        spr.setAdapter(arrayAdapter);
+
+    }
+
 }
--- app/src/main/java/com/todaylunch/testproject/MainActivity.java
+++ app/src/main/java/com/todaylunch/testproject/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn_singleton, btn_strategy;
+    private Button btn_singleton, btn_strategy, btn_factory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
         btn_singleton = findViewById(R.id.btn_singleton_main);
         btn_strategy = findViewById(R.id.btn_strategy);
+        btn_factory = findViewById(R.id.btn_factory_main);
 
 
         btn_singleton.setOnClickListener(new View.OnClickListener() {
@@ -46,6 +47,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btn_factory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, FactoryActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
 
 
